b2c4efb381ae7a0922f1b063a4d4a657ecf161bc
kevinQuispe <kquispe07@hotmail.com>
2018-12-10 11:31:15 -0500

    cambios ensosa lista

--- app/src/main/java/com/piuraservices/piuraservices/adapters/enosa/ListaInfoContactosEnosaAdapter.java
+++ app/src/main/java/com/piuraservices/piuraservices/adapters/enosa/ListaInfoContactosEnosaAdapter.java
@@ -1,4 +1,56 @@
 package com.piuraservices.piuraservices.adapters.enosa;
 
-public class ListaInfoContactosEnosaAdapter {
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.TextView;
+
+import com.piuraservices.piuraservices.R;
+import com.piuraservices.piuraservices.models.enosa.InfoContactosEnosamodel;
+import com.piuraservices.piuraservices.models.enosa.InfoReclamosEnosamodel;
+
+import java.util.ArrayList;
+
+public class ListaInfoContactosEnosaAdapter extends BaseAdapter {
+    private Context context;
+    protected ArrayList<InfoContactosEnosamodel> lista;
+
+    public ListaInfoContactosEnosaAdapter(Context context, ArrayList<InfoContactosEnosamodel> lista) {
+        this.context = context;
+        this.lista = lista;
+    }
+
+    @Override
+    public int getCount() {
+        return lista.size();
+    }
+    @Override
+    public InfoContactosEnosamodel getItem(int position) {
+        return lista.get(position);
+    }
+    @Override
+    public long getItemId(int position) {
+        return lista.get(position).getId();
+    }
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        View row = convertView;
+        if (row == null) {
+            LayoutInflater inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            row = inflater.inflate(R.layout.lista_info_reclamos_enosa, parent, false);
+        }
+        TextView textView = (TextView) row.findViewById(R.id.list_reclamos_enosa_text);
+        InfoContactosEnosamodel item = lista.get(position);
+        String nombreempresa = item.getNombreempresa();
+        String oficinalugar = item.getOficinalugar();
+        String direccion = item.getDireccion();
+        String horario = item.getHorario();
+        String telefono = item.getTelefono();
+        String tipoatencion = item.getTipoatencion();
+
+        textView.setText(oficinalugar);
+        return row;
+    }
 }
--- app/src/main/java/com/piuraservices/piuraservices/models/enosa/InfoContactosEnosamodel.java
+++ app/src/main/java/com/piuraservices/piuraservices/models/enosa/InfoContactosEnosamodel.java
@@ -1,4 +1,80 @@
 package com.piuraservices.piuraservices.models.enosa;
 
 public class InfoContactosEnosamodel {
+    private long id;
+    private String nombreempresa;
+    private String oficinalugar;
+    private String direccion;
+    private String telefono;
+    private String horario;
+    private String tipoatencion;
+
+    public InfoContactosEnosamodel() {
+    }
+
+    public InfoContactosEnosamodel(long id, String nombreempresa, String oficinalugar, String direccion, String telefono, String horario, String tipoatencion) {
+        this.id = id;
+        this.nombreempresa = nombreempresa;
+        this.oficinalugar = oficinalugar;
+        this.direccion = direccion;
+        this.telefono = telefono;
+        this.horario = horario;
+        this.tipoatencion = tipoatencion;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getNombreempresa() {
+        return nombreempresa;
+    }
+
+    public void setNombreempresa(String nombreempresa) {
+        this.nombreempresa = nombreempresa;
+    }
+
+    public String getOficinalugar() {
+        return oficinalugar;
+    }
+
+    public void setOficinalugar(String oficinalugar) {
+        this.oficinalugar = oficinalugar;
+    }
+
+    public String getDireccion() {
+        return direccion;
+    }
+
+    public void setDireccion(String direccion) {
+        this.direccion = direccion;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+
+    public String getHorario() {
+        return horario;
+    }
+
+    public void setHorario(String horario) {
+        this.horario = horario;
+    }
+
+    public String getTipoatencion() {
+        return tipoatencion;
+    }
+
+    public void setTipoatencion(String tipoatencion) {
+        this.tipoatencion = tipoatencion;
+    }
 }
